15010c84a5f53e3d2b782759ece94e5f799cfd79
AlexeyK1212 <161300105+AlexeyK1212@users.noreply.github.com>
2024-03-04 22:25:37 +0300

    Add files via upload

---
+++ Util/src/main/java/Fraction.java
@@ -0,0 +1,47 @@
+
+
+
+
+
+interface Fractionable {
+    @Cache
+    double doubleValue();
+    @Mutator
+    void setNum(int num);
+    @Mutator
+    void setDenum(int denum);
+}
+
+
+
+public class Fraction implements Fractionable {
+
+
+    private int num;
+    private int denum;
+
+    public Fraction(int num, int denum) {
+        this.num = num;
+        this.denum = denum;
+
+    }
+
+    public void setNum(int num) {
+        this.num = num;
+    }
+
+    public void setDenum(int denum) {
+        this.denum = denum;
+
+    }
+
+
+    @Override
+    public double doubleValue() {
+        System.out.println("invoke double value") ;
+        return (double) num/denum;
+    }
+
+
+
+}
---
+++ Util/src/test/java/UtilsTest.java
@@ -0,0 +1,49 @@
+import org.junit.jupiter.api.Test;
+
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class UtilsTest {
+
+
+    @Test
+    void cache() {
+        ByteArrayOutputStream outContent = new ByteArrayOutputStream();
+        PrintStream originalOut = System.out;
+        System.setOut(new PrintStream(outContent));
+
+        Fraction fr = new Fraction(1,2);
+        Utils ut =new Utils();
+        Fractionable num = ut.cache(fr);
+
+
+        assertEquals(num.doubleValue (),0.5);
+        assertEquals(outContent.toString(),"invoke double value\r\n");
+        outContent.reset();
+
+        assertEquals(num.doubleValue (),0.5);
+        assertEquals(outContent.toString(),"");
+        outContent.reset();
+
+        assertEquals(num.doubleValue (),0.5);
+        assertEquals(outContent.toString(),"");
+        outContent.reset();
+
+        num.setNum(5);
+        assertEquals(num.doubleValue (),2.5);
+        assertEquals(outContent.toString(),"invoke double value\r\n");
+        outContent.reset();
+
+        assertEquals(num.doubleValue (),2.5);
+        assertEquals(outContent.toString(),"");
+        outContent.reset();
+
+        System.setOut(originalOut);
+    }
+
+
+
+
+}
